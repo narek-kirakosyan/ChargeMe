b845d5367b4f52f6da7d54a92fbd9e183fa6442d
Narek-K <narekkirakosyan1991@icloud.com>
2025-08-06 21:21:43 +0400

    Initial Commit

---
+++ EVStation/EVStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVStationApp.swift
+//  EVStation
+//
+//  Created by Narek Kirakosyan on 06.08.25.
+//
+
+import SwiftUI
+
+@main
+struct EVStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EVStationTests/EVStationTests.swift
@@ -0,0 +1,17 @@
+//
+//  EVStationTests.swift
+//  EVStationTests
+//
+//  Created by Narek Kirakosyan on 06.08.25.
+//
+
+import Testing
+@testable import EVStation
+
+struct EVStationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
